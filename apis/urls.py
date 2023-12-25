1c83d9c0990150e95b1eca12ffb803b121b29c74
ccble <abaybakirov11@gmail.com>
2023-12-25 21:59:50 +0600

    checked by the pofessor commit

--- apis/urls.py
+++ apis/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import PostAPIView, PostDetails
 
 urlpatterns = [
-    path("", PostAPIView.as_view(), name="home"),
-    path("post/<int:pk>/", PostDetails.as_view(), name="post_detail"),
+    path("", PostAPIView.as_view(), name="api_post_list"),
+    path("post/<int:pk>/", PostDetails.as_view(), name="api_post_detail"),
     
 ]
